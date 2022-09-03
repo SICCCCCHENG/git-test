5e45c119f75f6090c7b93fa93a6339cb6a222c5a
sicheng <zsc0373@gmail.com>
2022-09-03 15:45:48 -0230

    5th master push commit

--- src/main/java/com/sicheng/git/GitTest.java
+++ src/main/java/com/sicheng/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("hello git from hot-fix");
         System.out.println("4th master commit");
         System.out.println("hot-fix 2nd commit");
+
+        System.out.println("push test!");
     }
 }
