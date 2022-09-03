6c20cf8b8ea3fcef68ff4566cf88e899537006b2
sicheng <zsc0373@gmail.com>
2022-09-03 15:13:28 -0230

    4th master commit

--- src/main/java/com/sicheng/git/GitTest.java
+++ src/main/java/com/sicheng/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git03!");
 
         System.out.println("hello git from hot-fix");
+        System.out.println("4th master commit");
     }
 }
