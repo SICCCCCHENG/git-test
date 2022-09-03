c5d170187575b1b53654d830ec5543addda5ddaa
sicheng <zsc0373@gmail.com>
2022-09-03 14:26:45 -0230

    hot-fix commit from IDEA

--- src/main/java/com/sicheng/git/GitTest.java
+++ src/main/java/com/sicheng/git/GitTest.java
@@ -3,5 +3,9 @@ package com.sicheng.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git02!");
+        System.out.println("hello git03!");
+
+        System.out.println("hello git from hot-fix");
     }
 }
